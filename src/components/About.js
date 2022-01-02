45a1ba5fe4307fdb371357cc765f50af85a40930
josiah-williams <josiahdamiwilliams@gmail.com>
2022-01-03 00:02:16 +0100

    updated fix for all

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import petar from "../images/petar.jpg";
+import blake from "../images/blake.jpg";
+import eoin from "../images/eoin.jpg";
+import john from "../images/john.jpg";
+import max from "../images/max.png";
+
+function About() {
+}
+export default About;
